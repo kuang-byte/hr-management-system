f0e3fdefc38f27ac615d5907634c474e20926406
Hao Kuang <kuanghaochina@gmail.com>
2019-01-06 22:14:54 -0500

    add spring boot admin

--- service-management/src/main/java/com/hao/practice/servicemanagement/ServiceManagementApplication.java
+++ service-management/src/main/java/com/hao/practice/servicemanagement/ServiceManagementApplication.java
@@ -1,16 +1,18 @@
 package com.hao.practice.servicemanagement;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class ServiceManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceManagementApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceManagementApplication.class, args);
+  }
 
 }
 
